741f5d3dfdaf251ee27b7686cc1bc51847d25871
Samsursekh <sekhsamsur44@gmail.com>
2023-01-20 23:57:58 +0530

    deleted some files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import "./App.css";
-import About from "./Components/About";
-import Contact from "./Components/Contact";
-import Home from "./Components/Home";
-import Navbar from "./Components/Navbar";
-import Project from "./Components/Project";
-import Skills from "./Components/Skills";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
-      <About />
-      <Project />
-      <Skills />
-      <Contact />
-      
+     <h1>My Prtfolio</h1>
     </div>
   );
 }
